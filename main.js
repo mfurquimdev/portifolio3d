10ca1bfae4934d1abb3f4ba0f00504fa09d64218
Mateus Furquim <furquim@devgrid.co.uk>
2021-09-30 13:15:18 -0300

    Rotate point of light around the center

--- main.js
+++ main.js
@@ -15,11 +15,39 @@ camera.position.setZ(30);
 renderer.render( scene, camera );
 
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
-const material = new THREE.MeshBasicMaterial( {color: "#ECEFF4", wireframe: true} );
+const material = new THREE.MeshStandardMaterial( {color: 0xECEFF4} );
 const torus = new THREE.Mesh( geometry, material );
 
 scene.add(torus);
 
+const pointLight = new THREE.PointLight(0xECEFF4)
+pointLight.position.set(10,10,10)
+
+scene.add(pointLight);
+
+// obj - your object (THREE.Object3D or derived)
+// point - the point of rotation (THREE.Vector3)
+// axis - the axis of rotation (normalized THREE.Vector3)
+// theta - radian value of rotation
+// pointIsWorld - boolean indicating the point is in world coordinates (default = false)
+// https://stackoverflow.com/questions/42812861/three-js-pivot-point/42866733#42866733
+function rotateAboutPoint(obj, point, axis, theta, pointIsWorld){
+    pointIsWorld = (pointIsWorld === undefined)? false : pointIsWorld;
+
+    if(pointIsWorld){
+        obj.parent.localToWorld(obj.position); // compensate for world coordinate
+    }
+
+    obj.position.sub(point); // remove the offset
+    obj.position.applyAxisAngle(axis, theta); // rotate the POSITION
+    obj.position.add(point); // re-add the offset
+
+    if(pointIsWorld){
+        obj.parent.worldToLocal(obj.position); // undo world coordinates compensation
+    }
+
+    obj.rotateOnAxis(axis, theta); // rotate the OBJECT
+}
 
 function animate() {
   requestAnimationFrame( animate );
@@ -27,6 +55,10 @@ function animate() {
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.001;
+
+
+  rotateAboutPoint(pointLight, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 1), 0.1, true);
+
   renderer.render( scene, camera );
 }
 
