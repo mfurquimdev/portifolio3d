38d8c67973dcb1cf44fd048f5df7e3a22c1950f1
Mateus Furquim <furquim@devgrid.co.uk>
2021-09-30 12:50:33 -0300

    Add rotating torus

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import * as THREE from 'three';
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg')
+});
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.setZ(30);
+
+renderer.render( scene, camera );
+
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+const material = new THREE.MeshBasicMaterial( {color: "#ECEFF4", wireframe: true} );
+const torus = new THREE.Mesh( geometry, material );
+
+scene.add(torus);
+
+
+function animate() {
+  requestAnimationFrame( animate );
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.001;
+  renderer.render( scene, camera );
+}
+
+animate()
